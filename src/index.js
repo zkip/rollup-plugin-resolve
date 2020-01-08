07e4a37b3353c0c35488419db8e868f0cf472477
zkip <zkiplan@qq.com>
2020-01-08 18:42:52 +0800

    Code refactoring.

--- src/index.js
+++ src/index.js
@@ -1,183 +1,172 @@
-import crypto from "crypto";
-import path, { isAbsolute, normalize } from "path";
 import { join, dirname, resolve } from "path";
 import { createFilter } from "rollup-pluginutils";
-import { isDir, isExists, tryResolve } from "./util";
+import { isDir, isExists, tryResolve, first } from "./util";
 import checkOptions from "./checkOptions";
 
 import preConfig from "./preConfig";
 import genVirtuaGrouplModuleMaker from "./genVirtualGroupModuleMaker";
+import { ResolveError, ERR_INTERGRATION_TARGET, ERR_VARIABLE_MISSING } from "./errors";
 
-const defaultRenamer = (name, id) => {
-	return (
-		name ||
-		path
-			.basename(id, path.extname(id))
-			.replace(/[-+*/:;.'"`?!&~|<>^%#=@[\]{}()\s\\]+([a-z]|$)/g, (match, c) =>
-				c.toUpperCase()
-			)
-	);
-};
+// more details from https://nodejs.org/api/modules.html#modules_folders_as_modules
+const isES6DirExport = dirpath => !!tryResolve(join(dirpath, "index"), ["js", "node"])
 
-const ImportMode = {
-	Normal: 0,
-	Destructuring: 1
-};
-
-// function genVirtualID(importee, importer) {
-// 	let uid = crypto
-// 		.createHash("md5")
-// 		.update(importee)
-// 		.digest("hex");
-// 	return path.join(path.dirname(importer), "_" + uid);
-// }
-
-// function resolve(importee, importer, { base, candidateExt }) {
-// 	if (importer) {
-// 		let { file } = analysisFile(importer);
-// 		if (file) {
-// 			importer = path.dirname(importer);
-// 		}
-// 	} else {
-// 		importer = process.cwd();
-// 	}
-// 	let rst = {
-// 		fulId: "",
-// 		mode: ImportMode.Normal,
-// 		isSupport: true,
-// 		isDir: false,
-// 		isExist: false
-// 	};
-
-// 	let isIntegration = false,
-// 		fulId = ""
-
-// 	if (!path.isAbsolute(importee)) {
-// 		if (importee.startsWith("~/")) {
-// 			// $HOME and /
-// 			rst.fulId = importee.replace("~", process.env.HOME);
-// 		} else if (importee.startsWith("@/")) {
-// 			// base
-// 			rst.fulId = importee.replace("@", base);
-// 		} else if (importee.startsWith("{") && importee.endsWith("}")) {
-// 			// intergration
-// 			rst = resolve(importee.slice(1, -1), importer, {
-// 				base,
-// 				candidateExt
-// 			});
-// 			rst.mode = ImportMode.Destructuring;
-// 		} else if (importee.startsWith("./") || importee.startsWith("../")) {
-// 			rst.fulId = path.join(importer, importee);
-// 		} else {
-// 			// resolve from modules
-// 		}
-// 	} else {
-// 		rst.fulId = importee;
-// 	}
-
-// 	{
-// 		let { exist, dir, file } = analysisFile(rst.fulId);
-// 		let ext = path.extname(rst.fulId);
-// 		if (dir) {
-// 			rst.isDir = true;
-// 			rst.isExist = true;
-// 		} else {
-// 			if (exist) {
-// 				if (ext) {
-// 					if (candidateExt.indexOf(ext.slice(1)) < 0) {
-// 						rst.isSupport = false;
-// 					} else {
-// 						rst.isExist = true;
-// 					}
-// 				} else {
-// 					// A file without extname
-// 					rst.isSupport = false;
-// 				}
-// 			} else {
-// 				rst.isSupport = false;
-// 				for (let ext of candidateExt) {
-// 					let fulId = rst.fulId + "." + ext;
-// 					if (analysisFile(fulId).exist) {
-// 						rst.fulId = fulId;
-// 						rst.isExist = true;
-// 						rst.isSupport = true;
-// 						break;
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return rst;
-// }
-
-const isES6DirExport = dirpath => isExists(join(dirpath, "index.js"))
+// more details from https://nodejs.org/api/modules.html#modules_file_modules
+const extensions = ["mjs", "js", "json", "node"]
 
 /*
-mode:
-	N Normal
-	I Intergration
-	C Combine
+	mode
+		I Intergration
+		C Combine
 */
-const genVirtualID = (target, mode = "N") => `\0${mode}::${target}`;
+const genVirtualID = (target, mode) => `\0${mode}::${target}`;
 const parseVirtualID = raw => {
+
 	if (raw.startsWith("\0")) {
+
 		let ret = raw.slice(1).split("::");
+
 		if (ret.length === 2) {
+
 			return { mode: ret[0], target: ret[1] }
+
 		}
+
 	}
+
 	return null;
+
 }
 
+const internalVariables = new Set("~", "@");
+
 export default (options = {}) => {
-	const { include, exclude, base, candidateExt, variables } = checkOptions(options);
+
+	const { include, exclude, base, candidateExt, variables, dirBehaviour } = checkOptions(options);
 	const filter = createFilter(include, exclude);
 
-	const getVirtualGroupModule = genVirtuaGrouplModuleMaker()
+	const getVirtualGroupModule = genVirtuaGrouplModuleMaker();
 
 	return {
 		name: "resolve",
-		async resolveId(importee, importer) {
-			if (!filter(importee)) {
-				return null;
-			}
+		async resolveId(importee) {
 
-			let dir_name = importer && dirname(importer) || ""
-			let fullfp = resolve(dir_name, importee)
+			if (!filter(importee)) return null;
+
+			let target = importee;
+			let fullfp = "";
+			let isIntergration = false;
 
 			if (importee.startsWith("{") && importee.endsWith("}")) {
-				fullfp = join(dir_name, importee.slice(1, -1))
-				if (isExists(fullfp) && isDir(fullfp)) {
-					// intergration import
-					return genVirtualID(fullfp, "I")
+
+				target = importee.slice(1, -1);
+				isIntergration = true;
+
+			}
+
+			const frags = importee.split("/");
+			const firstFrag = frags.shift();
+			let prefix = "";
+
+			if (internalVariables.has(firstFrag)) {
+
+				if (firstFrag === "@") {
+
+					prefix = base
+
+				} else if (firstFrag === "~") {
+
+					prefix = process.env.HOME
+
+				}
+
+				target = frags.join("/");
+
+			} else if (firstFrag.startsWith("$")) {
+
+				// Variable import
+				const variable = firstFrag.slice(1);
+
+				if (variables.has(variable)) {
+
+					prefix = variables.get(variable);
+
 				} else {
-					throw new Error(`@zrpls/resolve: Intergration import only works for directory.`)
+
+					throw new ResolveError(ERR_VARIABLE_MISSING, `The variable "${variable}" is not defined.`)
+
 				}
+
+				target = frags.join("/");
 			}
 
-			if (isExists(fullfp)) {
-				if (isDir(fullfp)) {
-					if (isES6DirExport(fullfp)) {
-						return null;
+			fullfp = join(prefix === "" ? process.cwd() : prefix, target);
+
+			let is_exsit = isExists(fullfp);
+			let is_dir = is_exsit && isDir(fullfp);
+
+			// console.log(firstFrag, target, fullfp, prefix, is_exsit, is_dir, "@@@@@@@@@@@@");
+
+			if (is_exsit) {
+
+				if (is_dir) {
+
+					let mode = "I";
+
+					if (!isIntergration) {
+
+						if (dirBehaviour === "collective" || (dirBehaviour === "auto" && !isES6DirExport(target))) {
+
+							mode = "C";
+
+						} else {
+
+							// es6 import
+							return null;
+
+						}
+
 					}
-					// combine import
-					return genVirtualID(fullfp, "C")
+
+					return genVirtualID(target, mode);
+
+				} else if (isIntergration) {
+
+					throw new ResolveError(ERR_INTERGRATION_TARGET, `Intergration import only works for directory.`)
+
+				} else {
+
+					return fullfp;
+
 				}
+
 			} else {
-				return tryResolve(fullfp, candidateExt)
+
+				const maybe = tryResolve(fullfp, [...extensions, ...candidateExt]);
+
+				if (maybe && !isDir(maybe)) return maybe;
+
+				return null;
+
 			}
 
-			return null;
 		},
+
 		async load(id) {
-			let ret = parseVirtualID(id);
-			if (ret) {
-				let { mode, target } = ret;
-				if (mode === "I") return await getVirtualGroupModule(target, true)
-				if (mode === "C") return await getVirtualGroupModule(target)
+
+			const parsedVID = parseVirtualID(id);
+
+			if (parsedVID) {
+
+				const { mode, target } = parsedVID;
+
+				return await getVirtualGroupModule(target, mode === "I")
+
 			}
+
 		}
+
 	};
+
 };
 
 export { preConfig };
