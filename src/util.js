62c6a0f9e1dff7ed77b3b474fef3a82c61fc813d
zkip <zkiplan@qq.com>
2020-01-08 18:26:44 +0800

    fine tuning

--- src/util.js
+++ src/util.js
@@ -2,26 +2,41 @@ import fs from "fs";
 import isVarName from "is-var-name";
 
 export function setByPath(obj, val, path) {
+
 	function _set(o, ks) {
+
 		if (ks.length > 1) {
+
 			let k = ks.shift();
+
 			o[k] || (o[k] = {});
+
 			_set(o[k], ks);
+
 		} else {
+
 			o[last(ks)] = val;
+
 		}
+
 	}
+
 	_set(obj, path.split(/\//g));
+
 }
 
-const defaultCandidateExt = ["js"];
+export const tryResolve = (target = "", exts = []) => {
 
-export const tryResolve = (target = "", candidateExt = defaultCandidateExt) => {
-	for (const ext of candidateExt) {
+	for (const ext of exts) {
+
 		const fullfp = target + "." + ext;
+
 		if (isExists(fullfp)) {
+
 			return fullfp;
+
 		}
+
 	}
 
 	return null;
@@ -43,3 +58,57 @@ export const dualEach = o => fn =>
 export const lessFirst = (a, b) => (a.length < b.length ? [a, b] : [b, a]);
 
 export const all$p = (...ps) => Promise.all(ps);
+
+
+export function nIntersection(...arrs) {
+
+	const ok = new Map();
+	const result = [];
+
+	for (let arr1 of arrs) {
+
+		for (let arr2 of arrs) {
+
+			if (arr1 === arr2) continue;
+
+			let left = arr1.length > arr2.length;
+			let arr = left ? arr1 : arr2;
+			let arra = left ? arr2 : arr1;
+
+			for (let v of arr) {
+
+				if (!ok.has(v)) {
+
+					ok.set(v, true);
+
+				} else if (!ok.get(v)) {
+
+					continue;
+
+				}
+
+				if (!arra.includes(v)) {
+
+					ok.set(v, false);
+
+				}
+
+			}
+
+		}
+
+	}
+
+	for (let [k, b] of ok.entries()) {
+
+		if (b) {
+
+			result.push(k);
+
+		}
+
+	}
+
+	return result;
+
+}
