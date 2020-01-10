274877b1d6da64733487258fb3864d0a8d2ad26c
0lightbee0 <lightbee@qq.com>
2020-01-11 04:08:05 +0800

    update some unit tests.

--- test/samples/combine.js
+++ test/samples/combine.js
@@ -6,87 +6,43 @@ import { copySync, ensureDirSync, removeSync } from "fs-extra";
 import nResolve from "@rollup/plugin-node-resolve";
 
 import resolve from "../..";
-import { dualEach, dualMap } from "../dist/util.cjs";
+import { dualEach, dualMap, all$p } from "../dist/util.cjs";
 
 process.chdir(join(process.cwd(), "test/fixtures/combine"));
 
 const gen = t => async (dirBehaviour, input, answer) => {
-
 	const bundle = await rollup({
 		plugins: [resolve({ dirBehaviour }), nResolve()],
 		input
 	});
 
-	try {
-		let { module } = await testBundle(t, bundle);
-		// t.is(module.exports.answer, answer);
-	} catch (e) {
-		console.log(e, "@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-	}
-
+	let { module } = await testBundle(t, bundle);
+	t.is(module.exports.answer, answer);
 };
 
-// test("option dirBehaviour default (es6)", async t => {
+test("option dirBehaviour default (es6)", async t => {
+	const find = gen(t);
 
-// 	const find = gen(t);
+	await find(undefined, "es6/find.js", 37);
+});
 
-// 	await find(undefined, "es6/find.js", 37);
+test("option dirBehaviour collective", async t => {
+	const find = gen(t);
 
-// });
+	await find("collective", "collective/find.js", 37);
+});
 
-// test("option dirBehaviour collective", async t => {
+test("option dirBehaviour auto", async t => {
+	const find = gen(t);
 
-// 	const find = gen(t);
-
-// 	await find("collective", "collective/find.js", 37);
-
-// });
-
-// test("option dirBehaviour auto", async t => {
-
-// 	const find = gen(t);
-
-// 	await find("auto", "auto/find.js", 88);
-
-// });
-
-
+	await find("auto", "auto/find.js", 88);
+});
 
 test("collective, export conflict", async t => {
-
-	// Object.entries()
-
-
-	dualMap({ a: 1 })(() => {
-		// console.log("LKJKKKKKKKKKKKKKKKKK");
-		throw "DDDDDDDDDDDDDDDDDDDDDDDDD";
-	}).catch((err) => {
-		console.log(err, "NNNNNNNNNNNNNNNNNN");
-	})
-	// await new Promise((rv, rj) => {
-	// 	throw "BBBBBBBBB";
-	// })
-
-	// const map = (arr, iter) => {
-	// 	for (let i = 0; i < arr.length; i++) {
-	// 		iter(arr[i], i, arr).catch(err => { throw err })
-	// 	}
-	// }
-
-	// try {
-	// 	map([1, 2], () => {
-	// 		throw "FFFFFFFFFFFFFFFFF";
-	// 	})
-	// } catch (err) {
-	// 	console.log(err, "NMMMMMMMMMMMMMMMmmm");
-	// }
-
-
-	// await find("collective", "collective/conflict.js", 109);
-
-	// try {
-	// } catch (err) {
-	// 	console.log(err.code, "@@@@@@@@@@@@@@@");
-	// }
-
+	const find = gen(t);
+	try {
+		await find("collective", "collective/conflict.js", 109);
+	} catch ({ pluginCode: code }) {
+		t.is(code, "EXPORT_CONFLICT");
+	}
 });
